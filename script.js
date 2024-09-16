59ad120ecf21a6887b02b1083e6c1657a151b318
StingySpoilero <dalaujon@gmail.com>
2024-09-16 13:17:48 -0500

    LAB-308

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function csv(String) {
     let row = [];
 
     for (let i = 0; i < String.length; i++) {
-        const console.log = String[i];
+         console.log = String[i];
 
         if (console.log === ',') {
          
@@ -30,32 +30,43 @@ function csv(String) {
 }
 
 
-const Data1 = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctors Assistant,26";
+let Data1 = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctors Assistant,26";
 csv(Data1);
 
 
-const Data2 = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+let Data2 = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
 csv(Data2);
 
 
 // Part 3:
-function dataTransform (startData) {
-    startData.forEach (el => 
-        ; 
-    const objectArray = []; 
-
-    for (let i = 1; i < startData.length; i++) { 
-        const obj = {}; 
-        for (let j = 0; j < header.length; j++) { 
-            obj[header[j]] = startData[i][j]; 
+function dataTransform(startData) {
+let newArray = [];
+let headings = [];
+    startData.forEach(el,i) ; {
+        if(i == 0){
+            el.forEach(el => {
+                el = el.toLowerCase();
+                headings.push(el)
+            })
+        } else {
+            console.log(el)
         }
-        objectArray.push(obj); 
     }
+       console.log(el) ; 
+    
+       
 
-    return objectArray; 
+    return newArray; 
 }
+{let personArray "42", "Bruce", "Knight", "41"
+let headings = ["ID", "Name", "Occupation", "Age"]
+}
+function makeObject(headings, personArray) {
+let newPerson = {};
 
-
+for (let i in headings){
+    newPerson[headings[i]] = personArray[i]
+}
 let result = [
     ["ID", "Name", "Occupation", "Age"],
     ["42", "Bruce", "Knight", "41"],
@@ -63,13 +74,13 @@ let result = [
     ["63", "Blaine", "Quiz Master", "58"],
     ["98", "Bill", "Doctors Assistant", "26"]
 ];
-
+}
 //Part 4:
-{const objectArray = transformToObjects(resultsArray);
-console.log(objectArray);
+{const newArray = transformToObjects(resultsArray);
+console.log(newArray);
 }
 
-let objectArray = [
+let newArray = [
     { id: "42", name: "Bruce", occupation: "Knight", age: "41" },
     { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
     { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
@@ -77,42 +88,42 @@ let objectArray = [
 ];
 
 
-objectArray.pop();
+newArray.pop();
 
-objectArray.splice(1, 0, { id: "48", name: "Barry", occupation: "Runner", age: "25" });
-
-
-objectArray.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" });
+newArray.splice(1, 0, { id: "48", name: "Barry", occupation: "Runner", age: "25" });
 
 
-console.log(objectArray);
+newArray.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" });
+
+
+console.log(newArray);
 
 
 let totalAge = 0;
 
-for (const obj of objectArray) {
+for (const obj of newArray) {
     totalAge += parseInt(obj.age, 10);
 }
 
 
-const averageAge = totalAge / objectArray.length;
+const averageAge = totalAge / newArray.length;
 
 
 console.log('Average Age:', averageAge);
 
 
 // Part 5:
-function transformToCSV(objectArray) {
+function transformToCSV(newArray) {
  
-    if (objectArray.length === 0) {
+    if (newArray.length === 0) {
         return '';
     }
 
    
-    const header = Object.keys(objectArray[0]).join(','); 
+    const header = Object.keys(newArray[0]).join(','); 
 
     
-    const csvRows = objectArray.map(obj => {
+    const csvRows = newArray.map(obj => {
         return Object.values(obj).join(','); 
     });
 
