dc65f4c641486e59d578d0bb8e1b6679756b14bc
StingySpoilero <dalaujon@gmail.com>
2024-09-16 13:53:42 -0500

    LAB-308

--- script.js
+++ script.js
@@ -31,11 +31,11 @@ function csv(String) {
 
 
 let Data1 = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctors Assistant,26";
-csv(Data1);
+(Data1);
 
 
 let Data2 = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
-csv(Data2);
+(Data2);
 
 
 // Part 3:
@@ -76,7 +76,7 @@ let result = [
 ];
 }
 //Part 4:
-{const newArray = startData(Array);
+{let newArray = startData(Array);
 console.log(Array);
 }
 
@@ -101,9 +101,6 @@ console.log(newArray);
 
 let totalAge = 0;
 
-for (Object.newArray) {
-    totalAge += parseInt(obj.age, 10);
-}
 
 
 let averageAge = totalAge / newArray.length;
